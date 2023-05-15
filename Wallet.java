83b53a187324868644830f4f8b64bdadd90d92f5
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-15 14:52:12 +0300

    Create Wallet.java

---
+++ Wallet.java
@@ -0,0 +1,36 @@
+public class Wallet {
+
+    private int money;
+    public Wallet(){
+    }
+    public double getMoney(){
+        return money;
+    }
+    public void setMoney(int money){
+        this.money=money;
+    }
+    public void setNewMoney(int  newMoney){money = newMoney;}//тут відбвається фінансова справа +\- так запропонувала ідє
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
