0bedcc7b1b247ded9ed3fc93abb500722543eaa8
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-18 19:37:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,25 @@
+import Helpers.Helper;
+
 public class Main {
     public static void main(String[] args) {
 
-        Casino casino =new Casino();
-        Wallet wallet = new Wallet();
-        Bar bar = new Bar();
-        wallet.setMoney(500);// ти задаєш суму з якою ти прийшов в казино
-
-        casino.setNameCasino("Крути поки не здохнеш");// назва казино
+        Casino casino =new Casino("Casinosososos");
+        Wallet wallet = new Wallet(100);
+        Bar bar = new Bar("Bar");
         casino.getNameCasino();
-
-        casino.playBlackjack(wallet,30);// граєш в гру енну кількість разів
-        casino.playBlackjack(wallet,50);
-
-        casino.playRoulette(wallet,90);//далі гражєш
-
-        bar.setNameBar("BAR");// назва бару
-        bar.getNameBar();
-
-        bar.showMenu();// меню
-        bar.order(wallet,1);// вибір позицій
-        bar.order(wallet,2);
+        casino.playBlackjack(wallet,30);
+        casino.playRoulette(wallet,50);
+        bar.showMenu();
         bar.order(wallet,7);
+
+
+
+
+
+
+
+
+
 
 
 
