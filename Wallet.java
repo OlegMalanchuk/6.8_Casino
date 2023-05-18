492eadfe304e815da7298aefce4259e9882eaa8d
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-05-18 19:37:30 +0300

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -1,15 +1,22 @@
+import Helpers.Helper;
+
 public class Wallet {
 
     private int money;
-    public Wallet(){
+    public Wallet(int money){
+        this.money=money;
     }
     public double getMoney(){
+
         return money;
     }
     public void setMoney(int money){
         this.money=money;
     }
     public void setNewMoney(int  newMoney){money = newMoney;}//тут відбвається фінансова справа +\- так запропонувала ідє
+
+
+
 
 
 
